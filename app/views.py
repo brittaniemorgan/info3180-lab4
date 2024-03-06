dfddbed0a7e827c693f7e086fa99108876b9c674
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2024-03-06 01:51:10 -0500

    Updates login route

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
-
+from werkzeug.security import check_password_hash
 
 ###
 # Routing for your application.
@@ -43,20 +43,44 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
+        username = form.username.data
+        password = form.password.data
 
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
-
-        # Gets user id, load into session
-        login_user(user)
+        user = UserProfile.query.filter_by(username=username).first()
+        if user and check_password_hash(password, user.password):
+            # Gets user id, load into session
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for("upload"))
+        else:
+            flash('Invalid username or password', 'error')
 
         # Remember to flash a message to the user
         return redirect(url_for("home"))  # The user should be redirected to the upload form instead
+    return render_template("login.html", form=form)
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        
+
+        # Query the database for a user with the provided username
+        
+
+        if user and user.check_password(password):  # Assuming you have check_password method in your UserProfile model
+            login_user(user)
+            flash('Login successful!', 'success')
+            return redirect(url_for("upload"))  # Redirect to the upload form or any desired route
+        else:
+            flash('Invalid username or password', 'error')
+
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
